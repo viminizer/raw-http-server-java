044f12bb40c677bbf4da68aa53a2e263f1d1553a
SHOKHRUKH TURSUNOV <str9703@gmail.com>
2026-05-04 17:09:26 +0900

    feat: extract headers and body

--- RawServer.java
+++ RawServer.java
@@ -7,6 +7,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RawServer {
 
@@ -17,6 +19,10 @@ public class RawServer {
       + "\r\n"
       + body;
 
+  public static void log(String str) {
+    System.out.println(str);
+  }
+
   public static void handleClientSocket(Socket socket) throws IOException {
     try {
       InputStream in = socket.getInputStream();
@@ -25,7 +31,50 @@ public class RawServer {
       BufferedReader reader = new BufferedReader(new InputStreamReader(in));
       BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
 
-      System.out.println("printed: " + reader.readLine());
+      String reqLine = reader.readLine();
+      String[] methodPath = reqLine.split(" ");
+      String method = methodPath[0];
+      String path = methodPath[1];
+      log("---method path: " + method + " " + path);
+
+      /*
+       * > POST / HTTP/1.1
+       * > Host: localhost:4455
+       * > User-Agent: curl/8.7.1
+       * > Accept: all
+       * > Content-Type: application/json
+       * > Authorization: Bearer mytoken123
+       * > Content-Length: 49
+       * 
+       */
+
+      // parse headers
+      Map<String, String> headers = new HashMap<String, String>();
+      String line = reader.readLine();
+      while (line != null && !line.isEmpty()) {
+        log("line: " + line + "\n");
+        int idx = line.indexOf(":");
+        String key = "";
+        String value = "";
+        if (idx == -1) {
+          key = line.toLowerCase().trim();
+          headers.put(key, value);
+        } else {
+          key = line.substring(0, idx).toLowerCase().trim();
+          value = line.substring(idx + 1).trim();
+          headers.put(key, value);
+        }
+        line = reader.readLine();
+      }
+
+      log("---header: " + headers.toString() + "\n");
+
+      // parse json body
+      int contentLength = Integer.parseInt(headers.get("content-length"));
+      char[] cbuf = new char[contentLength];
+      reader.read(cbuf, 0, contentLength);
+      log(String.valueOf(cbuf));
+
       writer.write(response);
       writer.flush();
       System.out.println("flushed");
